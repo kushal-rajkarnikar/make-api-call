63031734d8224e1f7aec9934ceb86928b955bdd2
kushal-rajkarnikar <kushal.rajkarnikar@gmail.com>
2024-02-26 15:35:54 +0545

    fix code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
-  // const [count, setCount] = React.useState(1);
+  const [count, setCount] = React.useState(1);
 
   console.log("get data from API");
 
-  fetch(`https://swapi.dev/api/people/2`)
-    .then((res) => res.json())
-    .then((data) => setStarWarsData(data));
+  // built in functions / methods;
+  // fetch() -> API hit
+  // then() ->
+  // json() ->
 
-  // React.useEffect(
-  //   function () {
-  //     console.log("Effect ran");
-  //     fetch(`https://swapi.dev/api/people/${count}`)
-  //       .then((res) => res.json())
-  //       .then((data) => setStarWarsData(data));
-  //   },
-  //   [count]
-  // );
+  // fetch();
+  // then();
+  // json();
+
+  // fetch(`https://swapi.dev/api/people/1`)
+  //   .then((res) => res.json())
+  //   .then((data) => setStarWarsData(data)); //=>setter function state variable
+
+  // console.log(starWarsData);
+
+  React.useEffect(
+    function () {
+      console.log("Effect ran");
+      fetch(`https://swapi.dev/api/people/${count}`)
+        .then((res) => res.json())
+        .then((data) => setStarWarsData(data));
+    },
+    [count]
+  );
 
   return (
+    // setterFunction((receive current value of state variable) => receivedVariable +/-)
+
     <div>
-      {/* <h2>The count is {count}</h2>
+      <h2>The count is {count}</h2>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>
         Get Next Character
-      </button> */}
-      <pre>{JSON.stringify(starWarsData, null, 10)}</pre>
+      </button>
+      {/* respects line breaks and spaces  */}
+
+      <pre>{JSON.stringify(starWarsData, null, 5)}</pre>
+      {/* maintains line breaks and spaces */}
+      {/* JS expressions = {} */}
     </div>
   );
 }
